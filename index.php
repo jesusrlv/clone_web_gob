653e5266da93782a5da94a5bbfda6b91d9545f96
jesusrlv <jesusrlvrojo@gmail.com>
2022-04-22 15:57:00 -0500

    navbar scroll

--- index.php
+++ index.php
@@ -12,14 +12,94 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- type font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;400&display=swap" rel="stylesheet"> 
 
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
 
 </header>
+
+<head>
+<nav class="navscroll navbar navbar-expand-md navbar-dark fixed-top">
+    <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Left</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Codeply</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
+    <div class="mx-auto order-0">
+        <a class="navbar-brand mx-auto" href="#">Navbar 2</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Right</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+    <!-- <nav class="navscroll navbar navbar-expand-md fixed-top mx-auto pt-5">
+      <div class="container-fluid mx-auto">
+        <a class="navbar-brand mb-3 w-25 text-end" href="#"><img src="images/logos/logo_veda_02.png" width="170" height="63" alt="" class="align-middle rounded" ></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="line"></span> 
+          <span class="line"></span> 
+          <span class="line" style="margin-bottom: 0;"></span>
+        </button>
+        <div class="collapse navbar-collapse p-2" id="navbarCollapse">
+         
+          <ul class="navbar-nav w-75 mb-2 mb-md-0 ml-auto">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#one">Programas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="rendicion_cuentas/rendicion_cuentas_css.html">Rendición de cuentas</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Transparencia</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Sala de prensa</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#"><i class="bi bi-question-circle-fill"></i></a>
+            </li>
+          </ul>
+            
+        </div>
+      </div>
+    </nav> -->
+ 
+</head>
 
 <body>
     <div class="container-fluid">
@@ -70,18 +150,15 @@
         <div class="row p-5">
             <div class="col-3 text-center align-content-middle my-auto">
                 <!-- <img src="images/logos/logo2.png" width="270" height="120" alt="" class="align-middle rounded" style="background-color:rgba(90,90,90,0.3)"> -->
-                <img src="images/logos/logo_veda_02.png" width="270" height="100" alt="" class="align-middle rounded" >
+                <!-- <img src="images/logos/logo_veda_02.png" width="270" height="100" alt="" class="align-middle rounded" > -->
             </div>
-            <div class="col-9 align-content-middle my-auto">
+            <!-- <div class="col-9 align-content-middle my-auto">
                 <ul class="nav justify-content-end align-middle pe-5 align-middle">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                     </li>
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#">Gobierno</a>
-                    </li> -->
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Programas</a>
+                        <a class="nav-link" href="#one">Programas</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="rendicion_cuentas/rendicion_cuentas_css.html">Rendición de cuentas</a>
@@ -96,12 +173,11 @@
                         <a class="nav-link" href="#"><i class="bi bi-question-circle-fill"></i></a>
                     </li>
                 </ul>
-            </div>
+            </div> -->
 
         </div>
 
-
- 
+        
 
     </div>
     <div class="container-marketing w-100">
@@ -166,7 +242,7 @@
         </div>
 
     </div>
-
+    <div id="one"></div>
     <div class="container-fluid bg-light">
             <p class="text-center pt-5" style=" font-family: 'Montserrat', sans-serif;
                 font-size:32px;">Últimas noticias</p>
@@ -279,3 +355,77 @@
 include('footer.php');
 ?>
 </html>
+<script>
+$("a[href^='#']").click(function(e) {
+	e.preventDefault();
+	
+	var position = $($(this).attr("href")).offset().top;
+
+	$("body, html").animate({
+		scrollTop: position
+	} /* speed */ );
+});
+
+</script>
+
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
+<script>
+      $(function(){
+      $(window).scroll(function(){
+         if ($(this).scrollTop() > 100) {
+          $('.nav-link').addClass("azul");
+          $('.navscroll').addClass("azul");
+          $('.navbar-brand').addClass("azul");
+         } else {
+          $(".nav-link").removeClass("azul");
+          $(".navscroll").removeClass("azul");
+          $(".navbar-brand").removeClass("azul");
+         }
+      });
+  });
+    </script>
+
+      <style>
+
+        .navbar-brand.azul{
+          color: #000000;
+        }
+        .navbar-brand{
+          color: #ffffff;
+        }
+
+        .nav-link{
+          color: #ffffff;
+        }
+        .nav-link.azul{
+          color: #000000;
+        }
+        .navscroll {
+          background-color:rgba(0, 0, 0, 0.0);
+          color:yellow;
+          transition:background-color 500ms, color 500ms;
+          text-decoration: none;
+      }
+
+      .navscroll.azul {
+          background-color:rgb(255, 255, 255);
+          color:white;
+          box-shadow: 0 2px 4px 0 rgba(0,0,0,.2);
+      }
+
+      .navbar-toggler{
+          width: 47px;
+          height: 34px;
+          background-color:rgba(255, 255, 255, 0.3);
+          border:none;
+      }
+
+      .navbar-toggler .line{
+          width: 100%;
+          float: left;
+          height: 2px;
+          background-color: #fff;
+          margin-bottom: 5px;
+      }
+      </style>
